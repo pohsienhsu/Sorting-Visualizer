c46d6e943b555edfc36d8b9411d38e930c596509
phsu40 <phsu40@gatech.edu>
2022-02-16 19:44:16 -0500

    added generated data bars

--- visualizer.py
+++ visualizer.py
@@ -7,11 +7,18 @@ class DrawConfigs:
     WHITE = 255, 255, 255
     GREEN = 0, 255, 0
     RED = 255, 0, 0
-    GREY = 128, 128, 128
     BG_COLOR = WHITE
+    
+    GRADIENTS = [
+        (128, 128, 128),
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
     
     SIDE_PAD = 100
     TOP_PAD = 150
+    
+    
     
     def __init__(self, width, height, data):
         self.width = width
@@ -30,6 +37,22 @@ class DrawConfigs:
         self.bar_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
         self.start_x = self.SIDE_PAD // 2
         
+
+def draw(draw_data):
+    draw_data.window.fill(draw_data.BG_COLOR)
+    draw_bars(draw_data)
+    pygame.display.update()
+    
+def draw_bars(draw_data):
+    data = draw_data.data
+    
+    for i, val in enumerate(data):
+        x = draw_data.start_x + i * draw_data.bar_width
+        y = draw_data.height - ((val - draw_data.min_val) * draw_data.bar_height)
+        color = draw_data.GRADIENTS[i % 3]
+        
+        pygame.draw.rect(draw_data.window, color, (x, y, draw_data.bar_width, draw_data.height))
+
 
 def generate_starting_data(n, min_val, max_val):
     data = []
@@ -54,11 +77,18 @@ def main():
     while run:
         clock.tick(60)
         
-        pygame.display.update()
+        draw(draw_data)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type != pygame.KEYDOWN:
+                continue
+            
+            if event.key == pygame.K_r:
+                data = generate_starting_data(n, min_val, max_val)
+                draw_data.set_list(data)
                 
     pygame.quit()
     
